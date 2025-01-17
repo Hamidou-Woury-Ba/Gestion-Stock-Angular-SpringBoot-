5f4ac40ee52e37c8ded31eb3e3e0a3537e34a4c2
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2025-01-17 12:50:46 +0000

    Changer de mot de passe

--- src/app/pages/profil/changer-mot-de-passe/changer-mot-de-passe.component.ts
+++ src/app/pages/profil/changer-mot-de-passe/changer-mot-de-passe.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Icons } from '../../../font-awesome-icons';
 
 @Component({
   selector: 'app-changer-mot-de-passe',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './changer-mot-de-passe.component.html',
   styleUrl: './changer-mot-de-passe.component.css'
 })
 export class ChangerMotDePasseComponent {
+
+  faCancel = Icons["faCancel"];
+  faSave = Icons["faSave"];
 
 }
--- src/app/pages/profil/page-profil/page-profil.component.ts
+++ src/app/pages/profil/page-profil/page-profil.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Icons } from '../../../font-awesome-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-profil',
@@ -9,9 +10,21 @@ import { Icons } from '../../../font-awesome-icons';
   templateUrl: './page-profil.component.html',
   styleUrl: './page-profil.component.css'
 })
-export class PageProfilComponent {
+export class PageProfilComponent implements OnInit{
 
   faPencilAlt = Icons["faPencilAlt"];
   faLock = Icons["faLock"];
+
+  constructor(
+    private route : Router
+  ) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  ModifierMotDePasse() : void{
+    this.route.navigate(['changer-mot-de-passe']);
+  }
 
 }
